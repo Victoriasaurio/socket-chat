62a081beb9ace4c98600980ec6779027e8518f99
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-01-10 19:58:44 -0600

    Contiene el front-end de la placación

---
+++ public/js/socket-chat.js
@@ -0,0 +1,58 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+
+//Parametro que revisa el envio del nombre
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y la sala son necesarios');
+}
+
+//Guardando nombre en la variable usuario
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log(resp);
+    });
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información || Siempre que la página se recargue
+// socket.emit('crearMensaje', {
+//     usuario: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+//Escuechar cambios del usuario cuando entra o sale del chat
+socket.on('listaPersona', function(personas) {
+
+    console.log('Servidor:', personas);
+
+});
+
+//Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
+    console.log('Mensaje Privado', mensaje);
+});
